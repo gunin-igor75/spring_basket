11cf4b07567decfb448d05c3508ba76f5f55d835
Igor Gunin <gunin-igor75@yandex.ru>
2022-10-12 16:38:14 +0600

    First commit

---
+++ src/main/java/sky/pro/spring_basket/SpringBasketApplication.java
@@ -0,0 +1,13 @@
+package sky.pro.spring_basket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBasketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBasketApplication.class, args);
+    }
+
+}
